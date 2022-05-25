5b7183f113eacfcf0a823909f4000abe7b465758
abhaysampath <abhaysampath@gmail.com>
2022-05-25 00:44:20 -0400

    Added colors
    Updated game to show guess state with Green and Yellow colors

--- wordle.py
+++ wordle.py
@@ -1,30 +1,40 @@
+from colorama import init
+from colorama import Fore, Back, Style
+
+def format_match( letter='X' ):
+   return Back.GREEN + letter + Back.RESET
+def format_contains( letter='X' ):
+   return Back.YELLOW + letter + Back.RESET
+
+init()
+print()
 prompt = 'Enter the Wordle word: '
 wordle = input(prompt).upper()
 print('\033[1A' + prompt + '\033[K')
+print()
+
 print("Instructions:")
-print("  X = Right letter, Right spot")
-print("  * = Right letter, Wrong spot")
-print("  _ = Wrong letter, Right spot")
-print("Guesses must contain {} letters".format(len(wordle)));
+print("  " + format_match() + " = Right letter, Right spot")
+print("  " + format_contains() + " = Right letter, Wrong spot")
+print("  - = Wrong letter, Right spot")
+print("Guesses must contain {} letters".format(len(wordle)))
 print()
 
 unsolved=1
 while unsolved:
    status = ['-']*len(wordle)
    print(''.join(status), end = "\r")
-   guess = input().upper
-   if(len(guess())!=len(wordle)):
-      print("Guesses must contain {} letters".format(len(wordle)));
-      continue
-   for i, s in enumerate(guess()):
-#   i = 0
-#   for s in guess():
-      if s == wordle[i]:
-         status[i] = 'X'
-      elif s in wordle:
-         status[i] = '*'
-#      i = i + 1
-   if all(ele == 'X' for ele in status):
+   guess = input().upper()
+   if guess == wordle: #all(ele == 'X' for ele in status):
       unsolved = 0
+   if(len(guess)!=len(wordle)):
+      print("Guesses must contain {} letters".format(len(wordle)))
+      continue
+   for i, s in enumerate(guess):
+      if s == wordle[i]:
+         status[i] = format_match(s)
+      elif s in wordle:
+         status[i] = format_contains(s)
    print(''.join(status))
    print()
+print("Congratulations! You have guessed the word correctly!!")
